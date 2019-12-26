5f2dd73bda2d69b1932e9a576868fa705a701c1a
raphael-souza <raphael3130@gmail.com>
2019-12-26 10:34:00 -0300

    #master - Criando primeiros componentes

---
+++ src/app/pages/categories/category-form/category-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryFormComponent } from './category-form.component';
+
+@NgModule({
+  declarations: [CategoryFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryFormModule { }
