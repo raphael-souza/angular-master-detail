c0a3754d0d464a0634166c51410f40731d3a7c21
raphael-souza <raphael3130@gmail.com>
2019-12-26 11:39:15 -0300

    #master criando list category

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     CategoriesRoutingModule
-  ]
+  ],
+  declarations: [ CategoryListComponent, CategoryFormComponent]
 })
 export class CategoriesModule { }
